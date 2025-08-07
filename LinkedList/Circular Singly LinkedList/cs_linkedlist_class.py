c49177ebe38a3eedf3eb4af4eb6cbab4ca1ed7cd
arunakamara <kamaraaruna198@gmail.com>
2025-08-07 14:39:27 +0530

    Append method

--- LinkedList/Circular Singly LinkedList/cs_linkedlist_class.py
+++ LinkedList/Circular Singly LinkedList/cs_linkedlist_class.py
@@ -5,37 +5,49 @@ class Node:
 
 
 class CSLinkedList:
-    # With value 
-    def __init__(self, value):
-        node = Node(value)
-        node.next = node
-        self.head = node
-        self.tail = node
-        self.length = 1
+    # def __init__(self, value):
+    #     """ With value """
+    #     node = Node(value)
+    #     node.next = node
+    #     self.head = node
+    #     self.tail = node
+    #     self.length = 1
 
-    # Without value
-    # def __init__(self):
-    #     self.head = None
-    #     self.tail = None
-    #     self.length = 0
+    
+    def __init__(self):
+        """Without value"""
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+
+    def append(self, value):
+        """
+        Adds a new node to the end of the list
+        Time Complexity - O(1)
+        SPace Complexity - O(1)
+        """
+
+        new_node = Node(value)
+
+        # Check if the node is empty
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node
+
+        else:
+            self.tail.next = new_node
+            new_node.next = self.head
+            self.tail = new_node
+
+        self.length += 1 
+
+
+csl = CSLinkedList()
+csl.append(10)
+csl.append(20)
+print(csl.head.value)
+print(csl.head.next.value)
+print(csl.tail.next.value == csl.head.value)
         
-# Create a Circular LinkedList with a value
-cl = CSLinkedList(10)
-
-# Print the head value
-print(f"Head: {cl.head.value}")
-
-# Print the tail value
-print(f"Tail: {cl.tail.value}")
-
-# Print the next value of the tail node which is the same as head
-print(f"Tail's next value: {cl.tail.next.value}")
-
-# Print the length
-print(f"Length: {cl.length}")
-
-
-# Create a Circular LinkedList without a value
-# cl1 = CSLinkedList()
-# print(cl1.head.value)       # Error no value
-# print(cl1.head)             # Returns None
