47e741895f4526b15adc3203d7942f522d86e028
arunakamara <kamaraaruna198@gmail.com>
2025-08-10 17:32:18 +0530

    Added empty doubly linkedlist contructor

--- LinkedList/Doubly LinkedList/doubly_linkedlist_class.py
+++ LinkedList/Doubly LinkedList/doubly_linkedlist_class.py
@@ -3,11 +3,20 @@ class Node:
         self.value = value
         self.next = None
         self.prev = None
+        
 
     def __repr__(self):
         return str(self.value)
         # return f"{self.prev} <- [{self.value}] -> {self.next}"
 
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    
 
 
 
