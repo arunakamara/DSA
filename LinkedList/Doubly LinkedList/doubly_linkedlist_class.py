4d19206f5c75d027196475766dc8c16a3d78afd4
arunakamara <kamaraaruna198@gmail.com>
2025-08-10 17:25:15 +0530

    Doubly LinkedList Class Initial commit

---
+++ LinkedList/Doubly LinkedList/doubly_linkedlist_class.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+    def __repr__(self):
+        return str(self.value)
+        # return f"{self.prev} <- [{self.value}] -> {self.next}"
+
+
+
+
