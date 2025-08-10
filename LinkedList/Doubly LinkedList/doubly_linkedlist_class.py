466e2ca876550f37002434df3527c0c328c1188d
arunakamara <kamaraaruna198@gmail.com>
2025-08-10 17:38:52 +0530

    append method

--- LinkedList/Doubly LinkedList/doubly_linkedlist_class.py
+++ LinkedList/Doubly LinkedList/doubly_linkedlist_class.py
@@ -15,7 +15,28 @@ class DoubleLinkedList:
         self.head = None
         self.tail = None
         self.length = 0
+            
 
+    def append(self, value):
+        """
+        Adds a new node to the end of the list
+        Time Complexity - O(1)
+        Space Complexity - O(1)
+        """
+        # Create a new node
+        new_node = Node(value)
+
+        # If the list is empty
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        
+        self.length += 1
     
 
 
