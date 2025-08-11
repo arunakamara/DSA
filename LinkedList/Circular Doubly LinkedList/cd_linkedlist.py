bf4465691f2e244a00fc35e924b192955f6a5c79
arunakamara <kamaraaruna198@gmail.com>
2025-08-11 23:00:41 +0530

    Circular Doubly LinkedList with an element

--- LinkedList/Circular Doubly LinkedList/cd_linkedlist.py
+++ LinkedList/Circular Doubly LinkedList/cd_linkedlist.py
@@ -8,8 +8,17 @@ class Node:
         return f"{self.prev} <-> {self.value} <-> {self.next}"
     
 class CDLinkedList:
-    def __init__(self):
-        """With no element"""
-        self.head = None
-        self.tail = None
-        self.length = 0
+    # def __init__(self):
+    #     """With no element"""
+    #     self.head = None
+    #     self.tail = None
+    #     self.length = 0
+
+    def __init__(self, value):
+        """With one element"""
+        node = Node(value)
+        node.next = node
+        node.prev = node
+        self.head = node
+        self.tail = node
+        self.length = 1
