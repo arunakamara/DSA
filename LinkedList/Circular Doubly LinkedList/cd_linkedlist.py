20df7681b74ecddd31793a2c9feda1e01f4ee345
arunakamara <kamaraaruna198@gmail.com>
2025-08-11 23:50:38 +0530

    Representation method

--- LinkedList/Circular Doubly LinkedList/cd_linkedlist.py
+++ LinkedList/Circular Doubly LinkedList/cd_linkedlist.py
@@ -5,20 +5,46 @@ class Node:
         self.prev = None
 
     def __repr__(self):
-        return f"{self.prev} <-> {self.value} <-> {self.next}"
+        return str(self.value)
     
 class CDLinkedList:
-    # def __init__(self):
-    #     """With no element"""
-    #     self.head = None
-    #     self.tail = None
-    #     self.length = 0
+    def __init__(self):
+        """With no element"""
+        self.head = None
+        self.tail = None
+        self.length = 0
 
-    def __init__(self, value):
-        """With one element"""
-        node = Node(value)
-        node.next = node
-        node.prev = node
-        self.head = node
-        self.tail = node
-        self.length = 1
+    # def __init__(self, value):
+        # """With one element"""
+        # node = Node(value)
+        # node.next = node
+        # node.prev = node
+        # self.head = node
+        # self.tail = node
+        # self.length = 1
+
+    def __repr__(self):
+
+        if not self.head:
+            return "Empty List"
+        
+        current = self.head
+        result = ''
+
+        while current:
+            result += str(current.value)
+            current = current.next
+
+            if current is self.head:
+                break
+            
+            result += ' <-> '
+        
+        return result
+
+    
+
+    
+
+
+l = CDLinkedList()
