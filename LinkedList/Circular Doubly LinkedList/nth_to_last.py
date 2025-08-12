49905b52f13f010f7609196812aac3f5829c2ba7
arunakamara <kamaraaruna198@gmail.com>
2025-08-13 00:39:49 +0530

    nth to the last

---
+++ LinkedList/Circular Doubly LinkedList/nth_to_last.py
@@ -0,0 +1,30 @@
+from linkedlist_quest import LinkedList 
+
+
+def nthToLast(ll, n):
+    """
+    Returns the nth element from the last
+    """
+
+    # Initialize both pointer to head
+    pointer1 = ll.head
+    pointer2 = ll.head
+
+    # Put both pointers n-node apart
+    for _ in range(n):
+        if pointer2 is None:
+            return None
+        pointer2 = pointer2.next
+
+    # Move both pointer one step ahead until the second pointer reaches the tail
+    while pointer2:
+        pointer1 = pointer1.next
+        pointer2 = pointer2.next
+
+    # Return pointer
+    return pointer1
+
+l = LinkedList()
+l.generate(10, 1, 20)
+print(l)
+print(nthToLast(l, 2))
