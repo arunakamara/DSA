525749675ff0c17fc2ee0f01e0373c5287537ab7
arunakamara <kamaraaruna198@gmail.com>
2025-08-18 19:11:55 +0530

    1 Dimension Array

---
+++ Arrays/1D_array/array1.py
@@ -0,0 +1,13 @@
+import array
+import numpy as np
+
+arr = array.array("i")
+print(type(arr))
+print(arr)
+
+array1 = array.array("i", [1, 2, 3, 4])
+print(type(array1))
+print(array)
+
+arr1 = np.array([1, 2, 4])
+print(arr1)
