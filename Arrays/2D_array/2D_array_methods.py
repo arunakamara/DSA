4ca9853224a99489be8549c525de4476b3a935c1
arunakamara <kamaraaruna198@gmail.com>
2025-08-18 22:55:06 +0530

    Access element method

--- Arrays/2D_array/2D_array_methods.py
+++ Arrays/2D_array/2D_array_methods.py
@@ -1,18 +1,33 @@
 from create_2D_array import TwoD_array
 import numpy as np
 
-def insert(arr, index, data, axis):
-    return np.insert(arr, index, data, axis)
-
-def main():
-    arr = TwoD_array
+# Insert Method
+def insert(arr, index=0, data=[], axis=0):
     index = int(input("Enter index: "))
     axis = int(input("Enter axis: "))
     data = list(map(int, input("Enter data: ").split()))
     if (axis == 0 and len(data) != len(arr[0])) or (axis == 1 and len(data) != len(arr)):
         raise ValueError(f"Invalid number of data entered. \nPlease enter a data of length {len(arr[0]) if axis == 0 else len(arr) }")
-    new_array = insert(arr, index, data, axis)
-    print(new_array)
+    return np.insert(arr, index, data, axis)
+
+# Access Method
+def accessElement(arr):
+    rowIndex = int(input("Enter the row index: "))
+    colIndex = int(input("Enter the column index: "))
+
+    if rowIndex >= len(arr) or colIndex >= len(arr[0]):
+        return f"Invalid Index"
+    return arr[rowIndex][colIndex]
+
+
+def main():
+    arr = TwoD_array
+    # new_array = insert(arr)
+
+    print(arr)
+
+    element = accessElement(arr)
+    print(element)
 
 
 if __name__ == "__main__":
