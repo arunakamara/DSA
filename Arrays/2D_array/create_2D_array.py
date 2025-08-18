4ad9aaf68857eb0ef989ac02af88060750895dd8
arunakamara <kamaraaruna198@gmail.com>
2025-08-18 20:42:34 +0530

    Append and insert

--- Arrays/2D_array/create_2D_array.py
+++ Arrays/2D_array/create_2D_array.py
@@ -4,7 +4,32 @@ import numpy as np
 TwoD_array = np.array([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]])
 
 def main():
+
     print(TwoD_array)
+
+    #Insert a row at index 0
+    print(np.insert(TwoD_array, 0, [1, 1, 1, 1], 0))
+
+    print()
+
+    #Insert a column at index 0
+    print(np.insert(TwoD_array, 0, [2, 2, 2], 1))
+
+    print()
+
+
+    #Append a new row
+    print(np.append(TwoD_array, [[3, 3, 3, 3]], 0))
+
+    print()
+
+    print(TwoD_array)
+
+    print()
+
+    # #Append a new column
+    print(np.append(TwoD_array, np.array([[4, 4, 4]]).reshape(-1, 1), axis=1))
+
 
 if __name__ == "__main__":
     main()
