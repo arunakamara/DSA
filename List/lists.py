3b4dd3473676a1652e599a4b7685cc7903e1fb6e
arunakamara <kamaraaruna198@gmail.com>
2025-08-19 09:43:52 +0530

    Udate method

--- List/lists.py
+++ List/lists.py
@@ -6,7 +6,6 @@ print(f"First item: {shoppingList[0]}")
 print(shoppingList[-1])
 print(shoppingList[-2])
 print(shoppingList[-3])
-print(-4)
 
 # Traverse
 for i in range(len(shoppingList)):
@@ -15,4 +14,22 @@ for i in range(len(shoppingList)):
 empty = []
 for i in empty:
     print("I am empty")
+
+
+
+
+# Update 
+myList = [1,2,3,4,5,6,7]
+print(myList)
+myList.insert(4,15)
+
+# append
+myList.append(55)
+
+# extend
+newList = [11,12,13,14]
+myList.extend(newList)
+print(myList)
+
+
 
