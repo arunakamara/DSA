35fa147a2b2c24539244d6b486485f4036946286
arunakamara <kamaraaruna198@gmail.com>
2025-08-19 09:49:00 +0530

    search method

--- List/lists.py
+++ List/lists.py
@@ -16,20 +16,28 @@ for i in empty:
     print("I am empty")
 
 
-
-
-# Update 
+# Update - O(n)
 myList = [1,2,3,4,5,6,7]
 print(myList)
 myList.insert(4,15)
 
-# append
+# append - O(1)
 myList.append(55)
 
-# extend
+# extend - O(n)
 newList = [11,12,13,14]
 myList.extend(newList)
 print(myList)
 
 
+#  Searching for an element in the List
+myList =  [10,20,30,40,50,60,70,80,90]
+
+def searchinList(list, value):
+    for i in list:
+        if i == value:
+            return list.index(value)
+    return 'The value does not exist in the list'
+
+print(searchinList(myList, 100))
 
