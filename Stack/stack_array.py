047c217ab7cb2ed99ffc8f566d0237232a4d149e
arunakamara <kamaraaruna198@gmail.com>
2025-08-19 22:21:54 +0530

    push operation

--- Stack/stack_array.py
+++ Stack/stack_array.py
@@ -6,7 +6,19 @@ class Stack:
         """
         self.items = []
 
+    def push(self, element):
+        """
+        Time Complexity - O(1)
+        Space Complexity - O(1)
+        """
+        self.items.append(element)
+
 my_stack = Stack()
+my_stack.push(10)
+my_stack.push(20)
+my_stack.push(30)
+print(my_stack.items)
+
 
 # Because we're using list no need to
 # track the length we can simply len() method
