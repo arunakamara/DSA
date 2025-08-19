a6c7a868029b262bab69f6133c634d000f56df6f
arunakamara <kamaraaruna198@gmail.com>
2025-08-19 23:51:17 +0530

    Stack Implemented with a linkedlist

---
+++ stack_linkedList.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    # def __init__(self, value):
+    #     """With a value"""
+    #     new_node = Node(value)
+    #     self.top = new_node
+    #     self.length = 1
+
+
+    def __init__(self):
+        """Without a value"""
+        self.top = None
+        self.length = 0
