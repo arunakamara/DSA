733144dbe15a03c8fe9fbf26e930956c20fdee35
arunakamara <kamaraaruna198@gmail.com>
2025-08-19 09:33:20 +0530

    Added List folder

---
+++ List/lists.py
@@ -0,0 +1,14 @@
+
+shoppingList = ['Milk', 'Cheese', 'Butter']
+
+# Accessing Element
+print(f"First item: {shoppingList[0]}")
+
+# Traverse
+for i in range(len(shoppingList)):
+    shoppingList[i] = shoppingList[i]
+    print(shoppingList[i])
+empty = []
+for i in empty:
+    print("I am empty")
+
