7b2808a62b505ca8775b67b69ad69f1c9a20aef6
arunakamara <kamaraaruna198@gmail.com>
2025-08-19 12:02:30 +0530

    Comment

--- List/missing_number.py
+++ List/missing_number.py
@@ -14,6 +14,9 @@ def missing_number(arr, n):
 print(missing_number([1, 2, 3, 4, 6], 6))
 
 def missing(arr, n):
+    """ 
+    Returns a list of missing numbers in the array
+    """
     return [ i for i in range(1, n+1) if i not in arr ]
 
 print(missing([1, 3, 5, 8, 10], 10))
