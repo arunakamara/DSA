8cee2f91bb39769ee40129b06b6dd037a16d7c34
arunakamara <kamaraaruna198@gmail.com>
2025-08-19 14:36:26 +0530

    Add Two Sum - LeetCode 1

---
+++ List/two_sum.py
@@ -0,0 +1,42 @@
+
+
+def main():
+    myList = [1, 2, 3, 2, 3, 4, 5, 6]
+    # print(find_pairs(myList, 6))
+    print(twoSum([2, 7, 11, 15], 9))
+
+
+def twoSum(arr, target):
+    """
+    Returns the indices of the two elements that add up to the target
+    """
+    seen = {}
+
+    complement = None
+
+    for i, num in enumerate(arr):
+        complement = target - num
+
+        if complement in seen:
+            return seen[complement], i
+        seen[num] = i
+
+
+
+def find_pairs(arr, target):
+    """
+    Returns a list of tuples with the indices of all pairs that sum up to the target value
+    """
+    pairs = []
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] == arr[j]: # To avoid adding same pairs to the list
+                continue
+            elif arr[i] + arr[j] == target:
+                pairs.append((i, j))
+    return pairs
+
+
+if __name__ == "__main__":
+    main()
+    
