6a63c692930c4ed2751b98286c70cf47b7c87ef0
arunakamara <kamaraaruna198@gmail.com>
2025-08-19 10:05:33 +0530

    Edited

--- List/avg_list.py
+++ List/avg_list.py
@@ -10,3 +10,17 @@ while (True):
     average = total / count
 					
 print('Average:', average)
+
+
+# Using list
+
+numlist = list() 
+while (True):
+    inp = input('Enter a number: ') 
+    if inp == 'done': break
+    value = float(inp)
+    numlist.append(value)
+					
+average = sum(numlist) / len(numlist) 
+print('Average:', average)
+
--- List/lists.py
+++ List/lists.py
@@ -59,3 +59,8 @@ print(myList)
 
 
 
+
+
+
+
+
