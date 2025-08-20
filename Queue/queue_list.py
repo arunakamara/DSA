66fbd0197b5591778ec2857e75f508ff171caa69
arunakamara <kamaraaruna198@gmail.com>
2025-08-21 01:05:22 +0530

    peek method

--- Queue/queue_list.py
+++ Queue/queue_list.py
@@ -19,8 +19,38 @@ class Queue:
         Space Complexity - O(1)
         """
         self.items.append(value)
+
         return f"The element {value} is inserted at the end of the queue."
+    
+    def dequeue(self):
+        if self.is_empty():
+            return "The queue is empty."
+        
+        return self.items.pop(0)
+    
+    def peek(self):
+        if self.is_empty():
+            return "The queue is empty"
+
+        return self.items[0]
+
     
 
 my_queue = Queue()
+my_queue.enqueue(10)
+my_queue.enqueue(20)
+my_queue.enqueue(30)
+
 print(my_queue)
+
+my_queue.dequeue()
+print(my_queue)
+# print(my_queue.enqueue(5))
+# print(my_queue)
+# print(my_queue.dequeue())
+# print(my_queue.dequeue())
+# print(my_queue.dequeue())
+# print(my_queue.dequeue())
+# print(my_queue)
+
+print(my_queue.peek())
