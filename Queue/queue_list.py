d18ef16e9bc901080a443d62d016569e82bf9385
arunakamara <kamaraaruna198@gmail.com>
2025-08-21 00:00:06 +0530

    Comments

--- Queue/queue_list.py
+++ Queue/queue_list.py
@@ -7,9 +7,17 @@ class Queue:
         return ' '.join(values)
     
     def is_empty(self):
+        """
+        Time Complexity - O(1)
+        Space Complexity - O(1)
+        """
         return len(self.items) == 0
     
     def enqueue(self, value):
+        """
+        Time Complexity - O(1) / O(n)
+        Space Complexity - O(1)
+        """
         self.items.append(value)
         return f"The element {value} is inserted at the end of the queue."
     
