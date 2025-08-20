92b7ddd67cfa114b3b9fa50e4a51592f8bce75bc
arunakamara <kamaraaruna198@gmail.com>
2025-08-20 23:53:54 +0530

    Added str method

--- Queue/queue_list.py
+++ Queue/queue_list.py
@@ -1,3 +1,7 @@
 class Queue:
     def __init__(self):
-        ...
+        self.items = []
+
+    def __str__(self):
+        values = [ str(x) for x in self.items ]
+        return ' '.join(values)
