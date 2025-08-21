af9b68d199dcbb1f9ec7b6025f6757261826ed53
arunakamara <kamaraaruna198@gmail.com>
2025-08-21 12:23:47 +0530

    enqueue method

--- Queue/queue_linkedList.py
+++ Queue/queue_linkedList.py
@@ -7,7 +7,7 @@ class Node:
         return f"{self.value}"
     
 
-class LinkedList:
+class Queue:
     def __init__(self):
         self.head = None
         self.tail = None
@@ -19,3 +19,29 @@ class LinkedList:
         while current:
             yield current
             current = current.next
+
+    def __repr__(self):
+        values = [ str(x) for x in self]
+        return ' '.join(values)
+    
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+        return f"{value} has been added to the end of the queue"
+    
+
+    
+
+
+q = Queue()
+q.enqueue(5)
+q.enqueue(6)
+q.enqueue(7)
+print(q)
