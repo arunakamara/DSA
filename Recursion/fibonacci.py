043db9b66c9def27705c774c631418e084983cc3
arunakamara <kamaraaruna198@gmail.com>
2025-08-23 02:11:16 +0530

    Fibonacci Series

---
+++ Recursion/fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    assert n >= 0 and isinstance(n, int), "Fibonacci number cannot be negative or non integer."
+
+    if n in [0, 1]:
+        return n
+    
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(7))
